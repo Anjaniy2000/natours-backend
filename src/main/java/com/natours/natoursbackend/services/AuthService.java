882ec11ea15f1f32b753cbd614eb53bae26cfb9b
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-07-13 21:52:11 +0530

    password-encoder added by Anjaniy

--- src/main/java/com/natours/natoursbackend/services/AuthService.java
+++ src/main/java/com/natours/natoursbackend/services/AuthService.java
@@ -8,6 +8,7 @@ import com.natours.natoursbackend.models.AppUser;
 import com.natours.natoursbackend.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.Optional;
@@ -21,6 +22,9 @@ public class AuthService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void register(RegisterRequest registerRequest) throws Exception {
 
         Optional<AppUser> appUserOptional = userRepository.findByEmail(registerRequest.getEmail());
@@ -28,7 +32,7 @@ public class AuthService {
             AppUser appUser = new AppUser();
             appUser.setName(registerRequest.getName());
             appUser.setEmail(registerRequest.getEmail());
-            appUser.setPassword(registerRequest.getPassword());
+            appUser.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
             appUser.setCreatedDate(Instant.now());
             userRepository.save(appUser);
         }else{
